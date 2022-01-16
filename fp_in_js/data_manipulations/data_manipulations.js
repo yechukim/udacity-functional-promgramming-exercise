4eac3f15f5a3dcc36979da8a9a884e7c7907c32b
yechu <yechukim217@gmail.com>
2022-01-16 21:39:43 +0900

    fix and add solution

--- fp_in_js/data_manipulations/data_manipulations.js
+++ fp_in_js/data_manipulations/data_manipulations.js
@@ -10,13 +10,14 @@ const nearEarthObjects = require('./nasa_near_earth_object_API.json');
 // 2. What was the average absolute magnitude of all the near earth objects in this data set? Return the average absolute_magnitude_h.
 // Hint - you can achieve this multiple ways, but the reduce method can be a little-known but cool way to find averages. To do it though, you'll need to use the initial_value argument
 // For some extra challenge try using reduce with the initial setting argument. To learn more about it, take a look at this page: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-const twoArrays = nearEarthObjects.near_earth_objects["2019-12-02"].concat(nearEarthObjects.near_earth_objects["2019-12-01"])
-const avg = twoArrays.reduce((acc,curr,number,whole )=> {
-       return acc + curr.absolute_magnitude_h / whole.length
-    }
-,0)
+const twoArrays =Object.values(nearEarthObjects.near_earth_objects).flat()
+const avg = twoArrays.reduce((acc, curr, i, arr)=> {
+   if(i+1===arr.length){ // when it's last item
+       return acc/arr.length // avg trick
+   }
+   return acc += curr['absolute_magnitude_h']
+},0)
 //console.log(avg)
-
 // Hazardous -----------------------------------------------
 // 3. A list of all objects (their id, name, max size in miles, and closest approach in miles)
 // that are labeled potentially hazardous
@@ -58,6 +59,13 @@ const lessItems = lessThanThat.map(item =>(
 // 5. Of all the near-earth objects for this date,
 // find the time that the asteroid with the nearest miss will be closest to earth.
 
-twoArrays.reduce((acc, curr)=> {
+const closest = twoArrays.reduce((closest, curr)=> {
+    const curDist = parseFloat(curr.close_approach_data[0].miss_distance.miles)
+    const closeDist = parseFloat(closest.close_approach_data[0].miss_distance.miles)
 
+    if(curDist > closeDist){
+        return closest
+    }
+    return curr
 })
+console.log(closest.close_approach_data[0].miss_distance.miles)
